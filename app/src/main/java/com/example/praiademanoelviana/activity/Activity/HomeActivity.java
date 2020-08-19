0329b0a9b15ad7604320e4a65413e3a7da5d9916
Adriano <developer.adrianoscjr@gmail.com>
2020-08-18 22:00:31 -0300

    Correção do toast

--- app/src/main/java/com/example/praiademanoelviana/activity/Activity/HomeActivity.java
+++ app/src/main/java/com/example/praiademanoelviana/activity/Activity/HomeActivity.java
@@ -1,10 +1,5 @@
 package com.example.praiademanoelviana.activity.Activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -12,15 +7,17 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.praiademanoelviana.R;
 import com.example.praiademanoelviana.activity.Adpter.AdapterEmpresa;
-import com.example.praiademanoelviana.activity.Adpter.AdapterProduto;
 import com.example.praiademanoelviana.activity.Helper.ConfiruFirebase;
 import com.example.praiademanoelviana.activity.lister.RecyclerItemClickListener;
 import com.example.praiademanoelviana.activity.model.Empresa;
-import com.example.praiademanoelviana.activity.model.Produto;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -29,7 +26,6 @@ import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 import com.miguelcatalan.materialsearchview.MaterialSearchView;
 
-import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -38,7 +34,7 @@ public class HomeActivity extends AppCompatActivity {
     private MaterialSearchView searchView;
     private RecyclerView recyclerEmpresa;
     private List<Empresa> empresas = new ArrayList<>();
-    private DatabaseReference  firebaRef;
+    private DatabaseReference firebaRef;
     private AdapterEmpresa adapterEmpresa;
     String categoria;
 
@@ -47,16 +43,12 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         inicializarComponentes();
-        firebaRef= ConfiruFirebase.getFirebase();
+        firebaRef = ConfiruFirebase.getFirebase();
         autenticacao = ConfiruFirebase.getFirebaseAutenticacao();
 
 
         /* Recupera a categoria passada na Intent */
         categoria = getIntent().getStringExtra("Categoria");
-
-        Toast.makeText(this, "CATEGORIA >>> " + categoria, Toast.LENGTH_SHORT).show();
-
-
 
         //Configurações Toolbar
         Toolbar toolbar = findViewById(R.id.toolbarNova);
@@ -71,122 +63,121 @@ public class HomeActivity extends AppCompatActivity {
 
 //Recupera produtos para empresa
         recuparEmpresas();
-searchView.setHint("Pesquisar Empresas");
-searchView.setOnQueryTextListener(new MaterialSearchView.OnQueryTextListener() {
-    @Override
-    public boolean onQueryTextSubmit(String query) {
+        searchView.setHint("Pesquisar Empresas");
+        searchView.setOnQueryTextListener(new MaterialSearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
 
 
-        return false;
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                pesquisarEmpresas(newText);
+                return true;
+            }
+        });
+
+        recyclerEmpresa.addOnItemTouchListener(
+                new RecyclerItemClickListener(
+                        this,
+                        recyclerEmpresa,
+                        new RecyclerItemClickListener.OnItemClickListener() {
+                            @Override
+                            public void onItemClick(View view, int position) {
+                                Empresa empresaSelecionada = empresas.get(position);
+                                Intent i = new Intent(HomeActivity.this, CartapioActivity.class);
+                                i.putExtra("empresas", empresaSelecionada);
+                                startActivity(i);
+                            }
+
+                            @Override
+                            public void onLongItemClick(View view, int position) {
+
+                            }
+
+                            @Override
+                            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                            }
+                        }
+                )
+        );
     }
 
-    @Override
-    public boolean onQueryTextChange(String newText) {
-        pesquisarEmpresas(newText);
-        return true;
-    }
-});
+    private void pesquisarEmpresas(String pesquisa) {
+        DatabaseReference empresasRef = firebaRef
 
-recyclerEmpresa.addOnItemTouchListener(
-        new RecyclerItemClickListener(
-                this,
-                recyclerEmpresa,
-                new RecyclerItemClickListener.OnItemClickListener() {
-                    @Override
-                    public void onItemClick(View view, int position) {
-                        Empresa empresaSelecionada= empresas.get(position);
-                        Intent i =new Intent(HomeActivity.this,CartapioActivity.class);
-                        i.putExtra("empresas",empresaSelecionada);
-                        startActivity(i);
-                    }
-
-                    @Override
-                    public void onLongItemClick(View view, int position) {
-
-                    }
-
-                    @Override
-                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-                    }
-                }
-        )
-);
-    }
-
-    private  void pesquisarEmpresas(String pesquisa){
-DatabaseReference empresasRef = firebaRef
-
-        .child("empresas");
+                .child("empresas");
 
         Query query = empresasRef.orderByChild("nome")
                 .startAt(pesquisa)
-                .endAt(pesquisa+"\uf8ff");
-query.addListenerForSingleValueEvent(new ValueEventListener() {
+                .endAt(pesquisa + "\uf8ff");
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
 
-    @Override
-    public void onDataChange(DataSnapshot dataSnapshot) {
-        empresas.clear();
-        for(DataSnapshot ds: dataSnapshot.getChildren()){
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                empresas.clear();
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
 
-            Empresa  e = ds.getValue(Empresa.class);
+                    Empresa e = ds.getValue(Empresa.class);
 
-            /* Filtra a empresa por categoria */
-            if (e.getCategoria().equals(categoria)) {
-                empresas.add(e);
+                    /* Filtra a empresa por categoria */
+                    if (e.getCategoria().equals(categoria)) {
+                        empresas.add(e);
+                    }
+
+                }
+
+                adapterEmpresa = new AdapterEmpresa(empresas);
+                recyclerEmpresa.setAdapter(adapterEmpresa);
+
+                adapterEmpresa.notifyDataSetChanged();
+
+
             }
 
-        }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
 
-        adapterEmpresa = new AdapterEmpresa(empresas);
-        recyclerEmpresa.setAdapter(adapterEmpresa);
-
-        adapterEmpresa.notifyDataSetChanged();
-
-
+            }
+        });
     }
 
-    @Override
-    public void onCancelled(DatabaseError databaseError) {
-
-    }
-});
-    }
-    private void recuparEmpresas(){
-     DatabaseReference empresaRef = firebaRef
-             .child("empresas");
+    private void recuparEmpresas() {
+        DatabaseReference empresaRef = firebaRef
+                .child("empresas");
 
 
+        empresaRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                empresas.clear();
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
 
-empresaRef.addValueEventListener(new ValueEventListener() {
-    @Override
-    public void onDataChange(DataSnapshot dataSnapshot) {
-        empresas.clear();
-        for(DataSnapshot ds: dataSnapshot.getChildren()){
+                    Empresa e = ds.getValue(Empresa.class);
 
-            Empresa  e = ds.getValue(Empresa.class);
+                    /* Filtra a empresa por categoria */
+                    if (e.getCategoria().equals(categoria)) {
+                        empresas.add(e);
+                    }
 
-            /* Filtra a empresa por categoria */
-            if (e.getCategoria().equals(categoria)) {
-                empresas.add(e);
+
+                }
+
+                adapterEmpresa = new AdapterEmpresa(empresas);
+                recyclerEmpresa.setAdapter(adapterEmpresa);
+
+                adapterEmpresa.notifyDataSetChanged();
             }
 
 
-        }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
 
-        adapterEmpresa = new AdapterEmpresa(empresas);
-        recyclerEmpresa.setAdapter(adapterEmpresa);
-
-        adapterEmpresa.notifyDataSetChanged();
-    }
-
-
-
-    @Override
-    public void onCancelled(DatabaseError databaseError) {
-
-    }
-});
+            }
+        });
 
 
     }
@@ -196,18 +187,19 @@ empresaRef.addValueEventListener(new ValueEventListener() {
 
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_usuario, menu);
-         MenuItem item = menu.findItem(R.id.menuPesquisa);
-         searchView.setMenuItem(item);
+        MenuItem item = menu.findItem(R.id.menuPesquisa);
+        searchView.setMenuItem(item);
         return super.onCreateOptionsMenu(menu);
     }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
-        switch (item.getItemId()){
-            case R.id.menuSair :
+        switch (item.getItemId()) {
+            case R.id.menuSair:
                 deslogarUsuario();
                 break;
-            case R.id.menuConfigurações :
+            case R.id.menuConfigurações:
                 abrirConfiguracoes();
                 break;
 
@@ -216,23 +208,23 @@ empresaRef.addValueEventListener(new ValueEventListener() {
         return super.onOptionsItemSelected(item);
     }
 
- private  void inicializarComponentes(){
-     searchView = findViewById(R.id.materialS);
-     recyclerEmpresa = findViewById(R.id.recyclerEmpresa);
+    private void inicializarComponentes() {
+        searchView = findViewById(R.id.materialS);
+        recyclerEmpresa = findViewById(R.id.recyclerEmpresa);
 
 
+    }
 
- }
-    private void deslogarUsuario(){
+    private void deslogarUsuario() {
         try {
             autenticacao.signOut();
             finish();
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private void abrirConfiguracoes(){
+    private void abrirConfiguracoes() {
         startActivity(new Intent(HomeActivity.this, ConfiguracoesUsuariosActivity.class));
     }
 
